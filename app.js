97e6ca3d927a5e5732ccb76ebd270a2b4027cc25
Alyssa Ravasio <alyssaravasio@Alyssas-MacBook-Air.local>
2013-06-25 15:49:45 -0700

    switch to for loop

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express       = require('express')
 app.get('/', function(req, res){
   res.send('/dirtymoney');
 });
+
 app.get('/dirtymoney', function(req, res){
   var candidatesURI = {
     host: 'api.followthemoney.org',
@@ -29,6 +30,7 @@ app.get('/dirtymoney', function(req, res){
       })
 
       var hitContributionsAPI = function(candidateID){
+        console.log(candidateID)
         var contributionsURI = {
           host: 'api.followthemoney.org',
           path: '/candidates.contributions.php?key=44154ac0d5a8a934977a29c1cd071326&imsp_candidate_id=' + candidateID
@@ -39,21 +41,51 @@ app.get('/dirtymoney', function(req, res){
             data += chunk;
           });
           res.on('end', function(){
-            console.log("!!!!!!!!!!!!!!!!")
             var contributionData = parser.toJson(data, {object: true});
             console.log(contributionData);
           });
         }); // http.get
       }
 
-      var throttledhitContributionsAPI = _.throttle(hitContributionsAPI, 100);
+      var throttledhitContributionsAPI = _.throttle(hitContributionsAPI, 500);
 
-      //trying to access for all 200 candidates
-      _.each(hawaiiCandidatesIDs, function(candidateID){
-        hitContributionsAPI(candidateID)
-      }); // each
+      console.log("Getting contributions for " +hawaiiCandidatesIDs.length+ " candidates.")
 
+      for (var i=0; i<hawaiiCandidatesIDs.length; i++){
+        
+        console.log(hawaiiCandidatesIDs[i])
+        throttledhitContributionsAPI(hawaiiCandidatesIDs[i])
+      }
 
+      // var fs = require('fs');
+      // fs.writeFile('public/data/caCities.json', JSON.stringify(caCitiesJSON),function(err) {
+      //   if(err) {
+      //     console.log(err);
+      //   }
+      // })
+
+      // donation event
+      //   legislator name
+      //   legislator party
+      //   legislator district
+      //   legislator chamber (house or senate)
+      //   donation date
+      //   donation amount
+      //   donator name
+      //   donator industry
+      //   donator sector
+
+      // they all fire at the same time
+      // _.each(hawaiiCandidatesIDs, function(candidateID){
+      //   console.log(candidateID)
+      //   throttledhitContributionsAPI(candidateID)
+      // }); // each
+
+      // also doesnt work
+      // var throttledhitContributionsAPI = _.throttle( function(hawaiiCandidatesIDs){
+      //   _.each( hawaiiCandidatesIDs, hitContributionsAPI );
+      // }, 2000);
+      // throttledhitContributionsAPI(hawaiiCandidatesIDs);
 
     }); // on('end')
   }); // http.get
